d1fb8712461776ab12409da832e2c8ecac192150
marcglm <marcghalem@hotmail.fr>
2020-07-15 10:59:20 +0200

    first commit

--- ESGI.DesignPattern.Projet/Checkout.cs
+++ ESGI.DesignPattern.Projet/Checkout.cs
@@ -35,5 +35,39 @@ namespace ESGI.DesignPattern.Projet
                 emailService.SubscribeUserFor(product);
             }
         }
+
+        public class CheckoutBuilder{
+            private Product _product;
+            private UserConfirmation _newsLetterSubscribed;
+            private UserConfirmation _termsAndConditionsAccepted;
+
+            private IEmailService _emailService;
+
+            public CheckoutBuilder WithProduct(Product product){
+                _product = product;
+                return this;
+            }
+
+            public CheckoutBuilder WithNewsletterSubscribed(UserConfirmation newsLetterSubscribed ){
+                _newsLetterSubscribed = newsLetterSubscribed;
+                return this;
+            }
+
+            public CheckoutBuilder WithTermsAndConditionsAccepted(UserConfirmation termsAndConditionsAccepted){
+                _termsAndConditionsAccepted = termsAndConditionsAccepted;
+                return this;
+            }
+
+            public CheckoutBuilder WithIEmailService(IEmailService emailService){
+                _emailService = emailService;
+                return this;
+            }
+            public Checkout Build(){
+                if (!_termsAndConditionsAccepted.Accepted) throw new OrderCancelledException(_product);
+            
+                return new Checkout(_product,_emailService);
+            }
+        }
+
     }
 }
