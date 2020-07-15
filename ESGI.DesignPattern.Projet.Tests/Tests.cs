1c47b8f1285978e922a4df6216ec7e46b1e6b6ec
John allou <49111696+joumichy@users.noreply.github.com>
2020-07-15 11:34:06 +0200

    Update Tests.cs

--- ESGI.DesignPattern.Projet.Tests/Tests.cs
+++ ESGI.DesignPattern.Projet.Tests/Tests.cs
@@ -1,16 +1,48 @@
-﻿using System;
+using System;
 using Xunit;
+
+
 
 namespace ESGI.DesignPattern.Projet.Tests
 {
+    
     public class Tests
     {
+        private readonly Product _product = new Product("phone");
+        private UserConfirmation _termsAndCondition = new UserConfirmation("Acceptez vous ?");
+    
+      
+
+        [Fact]
+        public void isAProduct()
+        {
+            var result = _product;
+            Assert.Equal("phone",_product.ToString());
+
+        }
+
+        [Fact]
+        public void isOrderCancelled(){
+           
+           _termsAndCondition.Accepted = false;
+            var result = _termsAndCondition.Accepted;
+            
+            var exception = new OrderCancelledException(_product);
+
+            Assert.NotNull(exception);
+            Assert.Contains(_product.ToString(), exception.ToString());
+
+        }
         [Fact]
         public void Checkout()
         {
             
+           //Checkout checkout = new Checkout(Prod);
         }
+      
 
     }
+    
+    
 }
 
